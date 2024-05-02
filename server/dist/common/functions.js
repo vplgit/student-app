309a0c9ff02e1877ff5e6cc2649e63080c59c340
Vishal Prakash Lawate <55680450+vplgit@users.noreply.github.com>
2024-05-02 14:42:06 +0530

    Delete server/dist directory

--- server/dist/common/functions.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.common_functions = void 0;
-exports.common_functions = {};
--- server/dist/database/tables.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tables = void 0;
-//common module for tables if changes required in names of tables can we changes at this place only
-exports.tables = {
-    USER_TABLE: "users",
-    API_LOG: "api_log",
-};
